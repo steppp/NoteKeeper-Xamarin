907c92bde81ae87307859ff33fb6b6800c55d32f
Stefano Andriolo <stefano@srand.it>
2021-07-30 17:40:09 +0200

    Change navigation, add note model

--- NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
+++ NoteKeeper/NoteKeeper/ViewModels/ItemsViewModel.cs
@@ -69,7 +69,7 @@ namespace NoteKeeper.ViewModels
 
         private async void OnAddItem(object obj)
         {
-            await Shell.Current.GoToAsync(nameof(NewItemPage));
+            await Shell.Current.GoToAsync(nameof(ItemDetailPage));
         }
 
         async void OnItemSelected(Item item)
--- NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
+++ NoteKeeper/NoteKeeper/Views/ItemDetailPage.xaml.cs
@@ -1,16 +1,41 @@
-﻿using NoteKeeper.ViewModels;
+﻿using System;
+using NoteKeeper.ViewModels;
 using System.ComponentModel;
+using NoteKeeper.Models;
 using Xamarin.Forms;
 
 namespace NoteKeeper.Views
 {
     public partial class ItemDetailPage : ContentPage
     {
+        public Note Note { get; set; }
+
         public ItemDetailPage()
         {
             InitializeComponent();
+            InitializeData();
 
-            BindingContext = new ItemDetailViewModel();
+            //BindingContext = new ItemDetailViewModel();
+            BindingContext = Note;
+        }
+
+        private void InitializeData()
+        {
+            Note = new Note
+            {
+                Heading = "Test note",
+                Text = "Text for a test note"
+            };
+        }
+
+        private void CancelMenuItem_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Cancel option", "Cancel was selected", "Accept Button", "Cancel Button");
+        }
+
+        private void SaveMenuItem_OnClicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Save option", "Save was selected", "Accept Button", "Cancel Button");
         }
     }
 }
